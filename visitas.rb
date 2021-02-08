ccaee2c7e0c43247ec78d56d3e87db3a4c7f7d22
Elian Rojas <impresosgosen@gmail.com>
2021-02-08 16:02:07 -0600

    Visits ready

---
+++ visitas.rb
@@ -0,0 +1,13 @@
+
+vistas = [1000, 800, 250, 300, 500, 2500]
+
+def promedio(visits_array)
+    counter = 0
+    visits_array.each do |vist|
+        if counter += vist
+        end
+    end
+    counter / visits_array.count
+end
+
+puts "Diary average of visits to web page: #{promedio(vistas)} "
